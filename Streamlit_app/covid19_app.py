3bcea25d29382b38c676190b79022c67edf52305
Priyank Shrivastava <priyankshrivastava5678@gmail.com>
2026-02-24 13:36:24 +0530

    Update covid19_app.py
    Add debug block for model loading

--- Streamlit_app/covid19_app.py
+++ Streamlit_app/covid19_app.py
@@ -3,14 +3,22 @@ import streamlit as st
 import os
 import joblib
 
-# loading model,scaler and columns
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-model = joblib.load(os.path.join(BASE_DIR, '..', 'Pickel_files', 'logistic_model.pkl'))
-scaler = joblib.load(os.path.join(BASE_DIR, '..', 'Pickel_files', 'scaler.pkl'))
-columns = joblib.load(os.path.join(BASE_DIR, '..', 'Pickel_files', 'columns.pkl'))
-
 st.set_page_config(page_title='Covid Risk Predictor',layout='centered')
+
+# loading model,scaler and columns
+import traceback
+
+try:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    model = joblib.load(os.path.join(BASE_DIR, '..', 'Pickel_files', 'logistic_model.pkl'))
+    scaler = joblib.load(os.path.join(BASE_DIR, '..', 'Pickel_files', 'scaler.pkl'))
+    columns = joblib.load(os.path.join(BASE_DIR, '..', 'Pickel_files', 'columns.pkl'))
+
+except Exception as e:
+    st.error("Model loading failed")
+    st.text(str(e))
+    st.text(traceback.format_exc())
 
 # Header
 st.title("🩺 COVID-19 Risk Prediction System")
